2819ff5fc9ff64f42d568acef7253debfb8edd41
Marco van Wieren <marco@wpo365.com>
2022-10-13 23:06:51 +0200

    Added a class for the root element plus several fixed several audit-issues

--- src/lib/AppLauncher.ts
+++ src/lib/AppLauncher.ts
@@ -1,23 +1,24 @@
 export class AppLauncher {
   public static getReactDomConfig(): IReactDomConfig {
-    const allScripts = document.getElementsByTagName('script');
-    const currScript = allScripts[allScripts.length - 1];
+    const allScripts = document.getElementsByTagName('script')
+    const currScript = allScripts[allScripts.length - 1]
 
     // Determine script context current URL
-    const currScriptSrc = currScript.getAttribute('src');
-    let scriptContextUrl = '';
+    const currScriptSrc = currScript.getAttribute('src')
+    let scriptContextUrl = ''
 
     if (!!currScriptSrc) {
-      scriptContextUrl = `${currScriptSrc.substr(0, currScriptSrc.lastIndexOf('/') + 1)}`;
+      scriptContextUrl = `${currScriptSrc.substr(0, currScriptSrc.lastIndexOf('/') + 1)}`
     }
 
-    const root = document.createElement('div');
+    const root = document.createElement('div')
     const rootId = Math.random()
       .toString(36)
       .replace(/[^a-z]+/g, '')
-      .substr(0, 5);
-    root.setAttribute('id', rootId);
-    currScript.parentNode.appendChild(root);
+      .slice(0, 5)
+    root.setAttribute('id', rootId)
+    root.classList.add('wpo365-app-root')
+    currScript.parentNode.appendChild(root)
 
     let env = {
       nonce: currScript.getAttribute('data-nonce'),
@@ -26,16 +27,16 @@ export class AppLauncher {
       props: currScript.getAttribute('data-props')
         ? JSON.parse(currScript.getAttribute('data-props'))
         : {}
-    };
+    }
 
     return {
       rootId,
       env
-    };
+    }
   }
 }
 
 export interface IReactDomConfig {
-  rootId: string;
-  env: any;
+  rootId: string
+  env: any
 }
